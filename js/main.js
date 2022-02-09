5b66eb5a949a735cdcb4992a4ef66f3936afcec6
ayminahmedpk <ayminahmedpk@gmail.com>
2022-02-09 07:34:10 +0500

    dev v0.1.3 - 'second submission - inmproved sass, calc'

--- js/main.js
+++ js/main.js
@@ -7,10 +7,9 @@ const displayElement = document.querySelector('.screen-text');
 window.calculator = new Calculator(displayElement);
 
 const buttonHandler = (e) => {
-    //console.log(e.currentTarget);
     window.calculator.processInput(e.currentTarget.getAttribute('data-key-value'));
 };
 
 document.querySelectorAll('.keypad button').forEach(btn => {
     btn.addEventListener('click', buttonHandler);
-})
+});
